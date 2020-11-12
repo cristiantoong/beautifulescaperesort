71b0bdc688502672ed6457b7844b263f48a4b206
cristiantoong <cristiantoong@gmail.com>
2020-11-12 19:57:16 +0800

    added scrolltotop

--- js/scripts.js
+++ js/scripts.js
@@ -79,3 +79,42 @@ $(window).on("scroll", function() {
 
     }
 });  
+
+// // Scroll To Top
+// $(document).ready(function() {
+//     // show hide button on scroll
+//     $(window).scroll(function() {
+//         if ($(this).scrollTop() > 200) {
+//             $('.scrollToTop').addClass('fade-in');
+//         } else {
+//             $('.scrollToTop').removeClass('fade-in');
+//         }
+//     });
+  
+//     // smooth scrolling to top
+//     $('.scrollToTop').click(function() {
+//         $('html,body').animate({
+//             scrollTop: 0
+//         })
+//     })
+//   });
+
+
+// Scroll To Top
+$(document).ready(function() {
+    // show hide button on scroll
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) {
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+    });
+  
+    // smooth scrolling to top
+    $('.scrollToTop').click(function() {
+        $('html,body').animate({
+            scrollTop: 0
+        })
+    })
+  });
